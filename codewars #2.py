f9cdf428ac808bead70cfe2145527a31e01ad3cb
Muna <90852442+muna-mohammed@users.noreply.github.com>
2022-11-14 15:12:12 +0400

    Add files via upload
    Convert a Boolean to a String

---
+++ codewars #2.py
@@ -0,0 +1,11 @@
+# codewars series #2
+# link:
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+
+# the question:
+def boolean_to_string(b):
+
+
+# my own solution:
+def boolean_to_string(b):
+    return str(b)

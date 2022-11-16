ef8bbc039cc2e550842956a8086f3cbb1919ba98
Muna <90852442+muna-mohammed@users.noreply.github.com>
2022-11-16 14:21:57 +0400

    Add files via upload
    Find the mistake with assignment operator

---
+++ codewars series #5.py
@@ -0,0 +1,28 @@
+
+
+# the link:
+#https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
+
+# the question is : 
+# Basic variable assignment
+
+'''
+instractions:
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+'''
+# question:
+a == "code"
+b == "wa.rs"
+name == a + b
+    
+# my own solution:
+a = "code"
+b = "wa.rs"
+name = str(a + b)
+ 
+ 
+# onther solution
+a = "code"
+b = "wa.rs"
+name = a + b #replaced '==' with '='

fdde86f868b40156dab708b7a4ec7108c800d09f
Muna <90852442+muna-mohammed@users.noreply.github.com>
2022-11-16 13:47:31 +0400

    Add files via upload
    print all keywords

---
+++ code series #3.py
@@ -0,0 +1,11 @@
+
+
+# the link:
+https://www.codewars.com/kata/634ac4e77611b9f57dff456d/train/python
+
+# the question is : 
+#Oh, so you like programming? Name all of the keywords!
+
+import keyword
+keywords = keyword.kwlist
+print(keywords)
